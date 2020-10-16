f3a191f56c398f676362a9fcb6ecc12d4bc643ff
mkareken <70339051+mkareken@users.noreply.github.com>
2020-10-16 19:10:41 -0400

    fixes and updates

--- Paddle/Face.gd
+++ Paddle/Face.gd
@@ -9,18 +9,18 @@ func _ready():
 	show_face()
 
 func _physics_process(_delta):
-	if get_node("/root/Ball").has_node("Ball"):
-		var ball = get_node("/root/Game/Ball/Ball")
-		$Pupil_left/Sprite.position.x = 10
-		$Pupil_right/Sprite.position.x = 10
+	if get_node("/root/Game/Ball Container").has_node("ball"):
+		var ball = get_node("/root/Game/Ball Container/ball")
+		$Pupil_left/Sprite.position.x = 35
+		$Pupil_right/Sprite.position.x = -35
 		$Pupil_left.look_at(ball.position)
 		$Pupil_right.look_at(ball.position)
 		var d = ((($Mouth.global_position.y - ball.global_position.y)/height)-0.2)*2
 		d = clamp(d, -1, 1)
 		$Mouth.scale.y = d
 	else:
-		$Pupil_left/Sprite.position.x = 0
-		$Pupil_right/Sprite.position.x = 0
+		$Pupil_left/Sprite.position.x = 35
+		$Pupil_right/Sprite.position.x = -35
 		$Mouth.scale.y = 1
 
 func show_face():
